5f0b6a74484e0d3d969091d3be3af38f96d52938
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2024-01-20 16:28:07 -0500

    Test using real question/answer data.

--- test_band_generator.py
+++ test_band_generator.py
@@ -10,6 +10,10 @@ from simple_band_generator import SimpleBandGenerator
 def questions_fixture():
     expected = ["What's the name of the city you grew up in?", "What's your pet's name?"]
     return expected
+@pytest.fixture()
+def answers_fixture():
+    answers = ["Tarzana", "Kevin"]
+    return answers
 
 
 def test_list_questions(questions_fixture):
@@ -17,8 +21,8 @@ def test_list_questions(questions_fixture):
     assert band_generator.list_questions() == questions_fixture
 
 
-def test_question_answer(questions_fixture):
+def test_question_answer(questions_fixture, answers_fixture):
     band_generator = SimpleBandGenerator()
     pairs = band_generator.ask_questions()
-    expected: List[QuestionAnswer] = [QuestionAnswer()]
+    expected: List[QuestionAnswer] = [QuestionAnswer(question=questions_fixture[0], answer=answers_fixture[0])]
     assert pairs == expected
